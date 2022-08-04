c0118d187d9b8079b27f7c95de038d000af926cb
choi_aerim <kqy951118@gmail.com>
2022-08-04 12:45:55 +0900

    블로그 포스트 작성

---
+++ blog/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length = 30)
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add = True)
+    update_at = models.DateTimeField(auto_now = True)
+
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+
+# Create your models here.
